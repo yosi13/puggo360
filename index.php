0cb2c2488e7497a5bcf8e255dabb96c459db404f
Martin <zuca_yosi@hotmail.com>
2017-02-21 15:22:25 +0100

    project creation
    Creation of the web project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alienware
+ * Date: 2/20/2017
+ * Time: 11:13 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+    </body>
+</html>
+
