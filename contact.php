93258aefbd780b1596bbe1d2ad9b3062b67f6621
Martin <zuca_yosi@hotmail.com>
2017-03-21 23:59:45 +0100

    Contact modif
    Boolean modif

--- contact.php
+++ contact.php
@@ -119,7 +119,7 @@
                 <!-- Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work. -->
                 <!-- WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address! -->
                 <!-- NOTE: To use the contact form, your site must be on a live web host with PHP! The form will not work locally! -->
-                <?php if(!($_POST['succes']=="yes")){ ?>
+                <?php if(($_POST['succes']=="yes")){ ?>
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h3 class="panel-title">Email sent</h3>
